a571ae18007e880b8d367269c84723ce82dda471
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2022-09-21 10:15:17 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(127)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.setVolume(0)
+})
+function servo2 () {
+    pins.servoWritePin(AnalogPin.P2, servo)
+    serial.writeString("50,distance." + makerbit.getUltrasonicDistance(DistanceUnit.CM) + "")
+}
+let servo = 0
+serial.setBaudRate(BaudRate.BaudRate115200)
+serial.redirectToUSB()
+let contador = 0
+servo = 0
+makerbit.connectUltrasonicDistanceSensor(DigitalPin.P1, DigitalPin.P0)
+serial.writeLine(".")
+serial.writeLine("50,distance." + makerbit.getUltrasonicDistance(DistanceUnit.CM) + "")
+loops.everyInterval(20, function () {
+    led.plotBarGraph(
+    servo,
+    180
+    )
+    if (contador < 180) {
+        contador += 1
+        servo += 1
+    } else if (contador >= 179 && contador < 360) {
+        contador += 1
+        servo += -1
+    } else {
+        contador = 0
+    }
+})
+basic.forever(function () {
+    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 10 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 20) {
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        servo2()
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 21 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 30) {
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        servo2()
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 31 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 40) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        servo2()
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 41 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 50) {
+        servo2()
+    } else {
+        servo2()
+    }
+})
